74fcda3d04acc0c2099762afacac127c57e441df
Hurrell <cem.hurrell@gmail.com>
2020-10-16 15:03:11 +0100

    Form CSS cleanup.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Tax API" });
+  res.render("index", { title: "Monies Due" });
 });
 
 module.exports = router;
